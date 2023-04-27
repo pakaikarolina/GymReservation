988e7f2c70001950981d1ec7ece0f4f7e84f3ea6
karolina_pakai <pakaikarolina@gmail.com>
2023-04-27 10:39:08 +0200

    DB config

--- src/main/java/com/karolinapakai/gymreservation/domain/coach/entity/Coach.java
+++ src/main/java/com/karolinapakai/gymreservation/domain/coach/entity/Coach.java
@@ -1,6 +1,8 @@
 package com.karolinapakai.gymreservation.domain.coach.entity;
 
+import com.karolinapakai.gymreservation.entity.Sport;
 import jakarta.persistence.*;
+import lombok.Builder;
 
 import java.util.List;
 
@@ -9,12 +11,13 @@ import java.util.List;
 public class Coach {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column
     private String name;
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Sport> sports;
 
 }
